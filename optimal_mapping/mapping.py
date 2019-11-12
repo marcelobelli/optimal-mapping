59fdc4442c276cd64dcad0e8f4f82fe58939e6f0
Marcelo Belli <marcelo@belli.me>
2019-11-12 18:46:17 -0300

    Create Mapping Class with row and col reductions and row scanning

--- optimal_mapping/mapping.py
+++ optimal_mapping/mapping.py
@@ -1,36 +1,68 @@
 # python 3.8.0
-from copy import deepcopy
-
-from geopy.distance import distance
-
-from optimal_mapping.helpers import get_csv_content
-
-def get_distance_between(cargo: dict, truck: dict) -> float:
-    return distance((cargo["origin_lat"], cargo["origin_lng"]), (truck["lat"], truck["lng"])).km
+import numpy as np
 
 
-def mapping(cargos_csv, trucks_csv):
-    cargos_data = get_csv_content(cargos_csv)
-    trucks_data = get_csv_content(trucks_csv)
+class Mapping:
+    def __init__(self, matrix):
+        self.matrix = np.array(matrix)
+        self.matrix_size = len(self.matrix)
+        self._chosen_zeros = list()
+        self._vertical_lines = list()
+        self._horizontal_lines = list()
+        self._zeros_scratched = set()
+
+    # Phase 1: Rows and Columns reductions
+    def _rows_reductions(self):
+        for row in self.matrix:
+            min_value = min(row)
+            for i in range(self.matrix_size):
+                row[i] -= min_value
+
+    def _columns_reductions(self):
+        for column in self.matrix.transpose():
+            min_value = min(column)
+            for i in range(self.matrix_size):
+                column[i] -= min_value
+
+    # Phase 2: Optimization of the problem
+    # Step 01: Draw a minimum numbers of lines to cover all the zeros of the matrix
+    # Procedure
+    # a) Row Scanning
+    # i) Starting from the first row, ask the question: is the exactly one zero in that row? If yes, make
+    # a square around that zero and draw a vertical line passing through that zero; Otherwise skip that row.
+    # ii) After scanning the last row, check whether all the zeros are covered with lines. If yes, go to Step 2;
+    # Otherwise, do column scanning.
+    # b) Column Scanning
+    # i) Starting from the first column, ask the question: is the exactly one zero in that column? If yes, make
+    # a square around that zero and draw a horizontal line passing through that zero; Otherwise skip that column.
+    # ii) After scanning the last column, check whether all the zeros are covered with lines.
+    #
+    # Step 02:
+
+    def _rows_scanning(self):
+        for x, row in enumerate(self.matrix):
+            zeros_from_row = [(x, y) for y in np.where(row == 0)[0]]
+            zeros_from_row = [zero for zero in zeros_from_row if zero not in self._zeros_scratched]
+
+            if len(zeros_from_row) != 1:
+                continue
+
+            chosen_zero = zeros_from_row.pop()
+            vertical_line = chosen_zero[1]
+            self._chosen_zeros.append(chosen_zero)
+            self._vertical_lines.append(vertical_line)
+
+            column = self.matrix.transpose()[vertical_line]
+            zeros_from_column = np.where(column == 0)[0]
+            self._zeros_scratched |= {(x, vertical_line) for x in zeros_from_column}
 
 
 
 
 
+    ########################## !!!!!!!!!!!!!!!! ATENCAO
+    # No scanning horizontal além da condicao if 0 < len((zero_index := np.where(column == 0)[0])) < 2: tem que ver o zero foi riscado.
+    # Pensando agora por esse mesmo motivo precisa guardar as posicões dos zeros sublinhados, pra não sublinhar duas vezes
 
-
-
-
-
-# def mapping(cargos: list, trucks: list):
-
-
-
-# Pra cada cargo faco o sorted dos trucks
-# .get no result, voltou pego o cargo e confiro qual dos dois conpensa ir com qual caminhão
-# Lembrando do caso.
-# C1 -> 5 -> P1
-# C1 -> 4 -> P2
-# C2 -> 10 -> P1
-# C3 -> 5 -> P2
-# Compensa mais C1 -> P1, C3 -> P2, totalizando 10 km
+    def _total_zero_values(self):
+        return self.matrix_size * self.matrix_size - np.count_nonzero(self.matrix)
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,16 +1,72 @@
-from optimal_mapping.mapping import get_distance_between
+import numpy as np
+import pytest
+
+from optimal_mapping.mapping import Mapping
 
 
-def test_get_distance_between():
-    cargo_data = {
-        "product": "Light bulbs",
-        "origin_lat": "36.876719",
-        "origin_lng": "-89.5878579",
-    }
-    truck_data = {
-        "truck": "Hartford Plastics Incartford",
-        "lat": "34.79981",
-        "lng": "-87.677251",
-    }
+@pytest.fixture
+def matrix():
+    return [
+        [14, 12, 15, 15],
+        [21, 18, 18, 22],
+        [14, 17, 12, 14],
+        [6, 5, 3, 6],
+    ]
 
-    assert get_distance_between(cargo_data, truck_data) == 287.9114022038136
+
+def test_rows_reductions(matrix):
+    expected_result = np.array([[2, 0, 3, 3], [3, 0, 0, 4], [2, 5, 0, 2], [3, 2, 0, 3]])
+    mapping = Mapping(matrix)
+    mapping._rows_reductions()
+
+    assert np.array_equal(mapping.matrix, expected_result)
+
+
+def test_columns_subtraction(matrix):
+    expected_result = np.array([[8, 7, 12, 9], [15, 13, 15, 16], [8, 12, 9, 8], [0, 0, 0, 0]])
+    mapping = Mapping(matrix)
+    mapping._columns_reductions()
+
+    assert np.array_equal(mapping.matrix, expected_result)
+
+
+def test_rows_scanning_doesnt_add_vertical_lines_if_zero_is_not_found():
+    matrix = np.array([[2, 4], [1, 9],])
+    mapping = Mapping(matrix)
+    mapping._rows_scanning()
+
+    assert mapping._vertical_lines == []
+
+
+def test_rows_scanning_doesnt_add_vertical_lines_if_finds_more_than_one_zero():
+    matrix = np.array([[2, 0, 4, 0, 1], [0, 1, 9, 0, 0]])
+    mapping = Mapping(matrix)
+    mapping._rows_scanning()
+
+    assert mapping._vertical_lines == []
+
+
+def test_rows_scanning():
+    matrix = np.array([[2, 4, 7, 2, 0], [0, 9, 7, 5, 4], [6, 7, 0, 0, 2], [2, 0, 1, 1, 0], [0, 5, 7, 1, 7]])
+    mapping = Mapping(matrix)
+    mapping._rows_scanning()
+
+    assert mapping._chosen_zeros == [(0, 4), (1, 0), (3, 1)]
+    assert sorted(mapping._vertical_lines) == [0, 1, 4]
+    assert mapping._zeros_scratched == {(0, 4), (3, 4), (1, 0), (4, 0), (3, 1)}
+
+
+def test_columns_scanning_doesnt_cadd_horizontal_lines_if_zero_is_not_found():
+    matrix = np.array([[2, 4], [1, 9]])
+    mapping = Mapping(matrix)
+    mapping._columns_scanning()
+
+    assert mapping._horizontal_lines == []
+
+
+def test_columns_scanning_doesnt_cadd_horizontal_lines_if_finds_more_than_one_zero():
+    matrix = np.array([[2, 4, 0, 1], [1, 9, 5, 0], [7, 1, 0, 0], [2, 1, 9, 0]])
+    mapping = Mapping(matrix)
+    mapping._columns_scanning()
+
+    assert mapping._horizontal_lines == []
